0ebfe84e523b8e0a66a716d831d8530466dfd42a
Ilya Kurachkin <patlatov@inbox.ru>
2022-05-18 23:41:31 +0300

    commit js_3

---
+++ js_3.js
@@ -0,0 +1,11 @@
+// Реализуйте функцию,
+//  которая преобразует данное логическое значение в его строковое представление.
+"use strict";
+
+function booleanToString(b) { 
+    if(b){
+      return "true";
+    }else{
+      return "false";
+    }
+  }
